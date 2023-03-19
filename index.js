3510e47e12f1a1e08cb3eac966f2104b9440bf9c
paulocfj <paulo.cfj2@gmail.com>
2023-03-19 20:12:38 -0300

    Menu - adicionando informação quando não há personagens

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-//import
 const prompt = require('prompt-sync')();
 
-//const
 const personagens = new Array();
 
 class Personagem {
@@ -23,6 +21,11 @@ function criarPersonagem() {
 }
 
 function listarPersonagens() {
+    if (personagens.length === 0) {
+        console.warn("A lista de personagens esta vazia!");
+        return; 
+    }
+
     personagens.forEach((personagem) => {
         console.log("Nome: ", personagem.nome);
         console.log("Raça: ", personagem.raca);
@@ -32,6 +35,11 @@ function listarPersonagens() {
 }
 
 function excluirPersonagem() {
+    if (personagens.length === 0) {
+        console.warn("Não há personagens a serem deletados!");
+        return; 
+    }
+
     const qtdPersonagens = personagens.length;
 
     personagens.forEach(({nome}, index) => {
@@ -52,47 +60,51 @@ function excluirPersonagem() {
     }
 }
 
-let opcao;
-do {
-    //Menu
-    console.clear();
-    console.log("--------------RPG----------------");
-    console.log("Escolha uma opção:");
-    console.log("1 - Criar Ficha");
-    console.log("2 - Escolher Ficha");
-    console.log("3 - Excluir Ficha");
-    console.log("0 - Sair");
-    console.log("---------------------------------");
-    opcao = parseInt(prompt("Informe uma opção valida: "));
+function menu() {
+    let opcao;
 
-    switch(opcao) {
-        case 0:
-            console.clear();
-            console.log("------------Até logo Aventureiro!--------------------");
-        break;
-        case 1:
-            console.clear();
-            console.log("------------Criação de Personagem----------");
-            criarPersonagem();
-            prompt("Aperte algum botão para continua! ");
-        break;
-        case 2: 
-            console.clear();
-            console.log("------------Escolha de Personagem---------");
-            listarPersonagens();
-            prompt("Aperte algum botão para continuar! ");
-        break;
-        case 3:
-            console.clear();
-            console.log("------------Exclusão de Personagem--------");
-            excluirPersonagem();
-            prompt("Aperte algum botão para continuar! ");
-        break
-        default:
-            console.clear();
-            console.warn("------------Opção invalida--------");
-            prompt("Aperte algum botão para continuar!");
-        break;
-    }
+    do {
+        console.clear();
+        console.log("--------------RPG----------------");
+        console.log("Escolha uma opção:");
+        console.log("1 - Criar Ficha");
+        console.log("2 - Escolher Ficha");
+        console.log("3 - Excluir Ficha");
+        console.log("0 - Sair");
+        console.log("---------------------------------");
+        opcao = parseInt(prompt("Informe uma opção valida: "));
+    
+        switch(opcao) {
+            case 0:
+                console.clear();
+                console.log("------------Até logo Aventureiro!--------------------");
+            break;
+            case 1:
+                console.clear();
+                console.log("------------Criação de Personagem----------");
+                criarPersonagem();
+                prompt("Aperte algum botão para continua! ");
+            break;
+            case 2: 
+                console.clear();
+                console.log("------------Escolha de Personagem---------");
+                listarPersonagens();
+                prompt("Aperte algum botão para continuar! ");
+            break;
+            case 3:
+                console.clear();
+                console.log("------------Exclusão de Personagem--------");
+                excluirPersonagem();
+                prompt("Aperte algum botão para continuar! ");
+            break
+            default:
+                console.clear();
+                console.warn("------------Opção invalida--------");
+                prompt("Aperte algum botão para continuar!");
+            break;
+        }
+    
+    } while(opcao != "0");
+}
 
-} while(opcao != "0");
+menu();
