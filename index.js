1d3496ec84a4abda2dbff4b00490bf8241beb192
paulocfj <paulo.cfj2@gmail.com>
2023-03-19 18:20:06 -0300

    Menu de usuário - visual

---
+++ index.js
@@ -0,0 +1,7 @@
+//Menu
+console.log("--------------RPG----------------");
+console.log("Escolha uma opção:");
+console.log("1 - Criar Ficha");
+console.log("2 - Escolher Ficha");
+console.log("3 - Excluir Ficha");
+console.log("---------------------------------");
